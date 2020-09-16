44bcae6b209d9d6fd392d553b8d3dd858b3af775
mariusiacob1 <iacob.marius10@gmail.com>
2020-09-15 21:04:19 -0700

    Update Array

--- CPSC_131_Array/Array.h
+++ CPSC_131_Array/Array.h
@@ -7,15 +7,85 @@ class Array
 {
 public:
 
-	Array(int size, T default_value)
+	Array()		// Default Constructor
+	: elements_(new T[0]), size_(0) { }
+
+	Array(int size, T default_value)	// Initialization
 	: elements_(new T [size]), size_(size)
 	{
+		if (size < 0)
+		{
+			throw std::out_of_range("Array::Array size must not be negative");
+		}
+
 		for (int i = 0; i < size_; i++)
 		{
 			elements_[i] = default_value;
 		}
 	}
+
+	Array(const Array& other) 
+	: elements_(new T[other.size_]), size_(other.size_)
+	{ 
+		for (int i = 0; i < size_; i++) 
+		{ 
+			elements_[i] = other.elements_[i]; 
+		} 
+	}
+
+	~Array()	// Destructor
+	{
+		delete [] elements_;
+	}
+
+	Array& operator = (const Array& rhs)
+	{
+		delete[] elements_;
+
+		size_ = rhs.size_;
+		elements_ = new T[rhs.size_];
+
+		for (int i = 0; i < size_; i++)
+		{
+			elements_[i] = rhs.elements_[i];
+		}
+		return *this;
+	}
+
+	int Size()	// Returns the size
+	{
+		return size_;
+	}
+
+	T Get(int i)	// Return the value x[i]
+	{
+		if ((i < 0) || (i >= Size()))	// When i is out of range, it throws a range_error
+		{
+			throw std::out_of_range("Array::Get index i out of range");
+		}
+
+		return elements_[i];
+	}
+
+	void Set(int i, T x)
+	{
+		if ((i < 0) || (i >= Size())) // When i is out of range, it throws a range_error
+		{
+			throw std::out_of_range("Array::Set index i out of range");
+		}
+		elements_[i] = x;
+	}
+
+	void Add(int i, T x)
+	{
+		throw std::overflow_error("Array:Add cannot add any elements");
+	}
 	
+	void Remove(int i, T x)
+	{
+		throw std::underflow_error("Array:Remove cannot remove any elements");
+	}
+
 private:
 	T* elements_;
 	int size_;
--- CPSC_131_Array/Array_test.cpp
+++ CPSC_131_Array/Array_test.cpp
@@ -10,5 +10,36 @@ int main(int argc, char* argv[])
 
 	Array<int> a2(5, 1);
 
+	cout << "a1 size: " << a1.Size() << endl;
+	cout << "a2 size: " << a2.Size() << endl;
+
+	for (int i = 0; i < a2.Size(); i++)
+	{
+		cout << "Get(i): " << a2.Get(i) << endl;
+	}
+
+	a2.Set(1, 99);
+	cout << "--" << endl;
+
+	for (int i = 0; i < a2.Size(); i++)
+	{
+		cout << "Get(i): " << a2.Get(i) << endl;
+	}
+
+	try
+	{
+		a2.Get(1000 * 100);
+	}
+	
+	catch (std::out_of_range e)
+	{
+		cout << "a2.Get threw an exception, what is " << e.what() << endl;
+	}
+
+	Array<int> a3(a2);		// Copy constructor
+	
+	Array<int> a4;
+	a4 = a3;
+
 	return 0;
 }
